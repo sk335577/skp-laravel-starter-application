9266af57b99da33826a74feabb6c8ddb7ca7c758
Sandeep <sandeep335577@gmail.com>
2021-07-04 22:05:04 +0530

    Add Project API Backend Frontend directories

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -8,8 +8,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
 
-class RouteServiceProvider extends ServiceProvider
-{
+class RouteServiceProvider extends ServiceProvider {
+
     /**
      * The path to the "home" route for your application.
      *
@@ -33,19 +33,33 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
+    public function boot() {
         $this->configureRateLimiting();
 
         $this->routes(function () {
             Route::prefix('api')
-                ->middleware('api')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/api.php'));
+                    ->middleware('api')
+                    ->namespace($this->namespace)
+                    ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/web.php'));
+                    ->namespace($this->namespace)
+                    ->group(base_path('routes/web.php'));
+
+//            // backend namespace
+//            $router->group(['namespace' => 'App\Http\Controllers\BackEnd'], function ($router) {
+//                require app_path('Http/routes-backend.php');
+//            });
+//
+//            // frontend namespace
+//            $router->group(['namespace' => 'App\Http\Controllers\FrontEnd'], function ($router) {
+//                require app_path('Http/routes-frontend.php');
+//            });
+//
+//            // api namespace
+//            $router->group(['namespace' => 'App\Http\Controllers\API'], function ($router) {
+//                require app_path('Http/routes-api.php');
+//            });
         });
     }
 
@@ -54,10 +68,10 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    protected function configureRateLimiting()
-    {
+    protected function configureRateLimiting() {
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
         });
     }
+
 }
